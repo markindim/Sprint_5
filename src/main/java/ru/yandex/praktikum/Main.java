88358e4fc2ca570770ad6d25925b254cc6767fad
dmitriy.markin <markindim@gmail.com>
2024-01-15 18:01:04 +0300

    upd code

--- src/main/java/ru/yandex/praktikum/Main.java
+++ src/main/java/ru/yandex/praktikum/Main.java
@@ -6,18 +6,20 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> farmAnimals = new ArrayList<>();
-        farmAnimals.add("DoG: Бобик");
-        farmAnimals.add("сat: Васька");
-        farmAnimals.add("COW: Буренка");
-        farmAnimals.add("dog: Шарик");
-        farmAnimals.add("cAt: Рыжик");
-        farmAnimals.add("sheep: Долли");
-        farmAnimals.add("cow: ");
+        farmAnimals.add("DoG Бобик");
+        farmAnimals.add("сat Васька");
+        farmAnimals.add("COW Буренка");
+        farmAnimals.add("dog Шарик");
+        farmAnimals.add("cAt Рыжик");
+        farmAnimals.add("sheep Долли");
+        farmAnimals.add("cow");
         farmAnimals.add("Жучка");
+        farmAnimals.add("not_defined Коровушка");
+        farmAnimals.add("cat l");
 
         AnimalFarm animalFarm = new AnimalFarm(farmAnimals);
 
         HashSet<String> uniqueNames = animalFarm.uniqueNames();
-        System.out.println("Unique animal names: " + uniqueNames);
+        System.out.println("Имена животных на ферме: " + uniqueNames);
     }
 }
